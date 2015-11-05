75c3839b8b6d89a4799956c75556496818b57702
SunNy <sunny013@bitbucket.org>
2015-11-05 15:57:24 +0600

    ballot validation

--- resources/views/form/ballot.blade.php
+++ resources/views/form/ballot.blade.php
@@ -4,24 +4,39 @@
 
 @section('content')
 Please chose 4 candidates. Else, your vote won't be counted.
-    <form action="/ballot?key={{ app('request')->input('key') }}" method="POST">
+    <form name="form" action="/ballot?key={{ app('request')->input('key') }}" method="POST">
         {!! csrf_field() !!}
 
-        {{--@foreach ($candidates as $candidate)--}}
-            {{--<div class="checkbox">--}}
-                {{--<input type="checkbox" name="candidate" value="{{$candidate->id}}"> {{$candidate->first_name}} {{$candidate->middle_name}} {{$candidate->last_name}}--}}
-            {{--</div>--}}
-        {{--@endforeach--}}
-
+        <?php $index=0; ?>
         @foreach ($candidates as $candidate)
-            <div class="checkbox">
-                <input tabindex="1" type="checkbox" name="candidate[]" value={{$candidate->id}}>{{$candidate->first_name}} {{$candidate->middle_name}} {{$candidate->last_name}}
+            <div class="checkbox checkbox-success" >
+                <input tabindex="1" class="styled" onclick="chkcontrol({{$index}})" id="checkbox"  type="checkbox" name="candidate[]" value={{$candidate->id}}>{{$candidate->first_name}} {{$candidate->middle_name}} {{$candidate->last_name}}
             </div>
+            <?php $index++; ?>
         @endforeach
 
-        <br>
-
-        <button type="submit" class="btn btn-default">Submit</button>
+        <button type="submit" id="vote" class="btn btn-success" disabled>VOTE</button>
     </form>
+
+    <script type="text/javascript">
+        function chkcontrol(j) {
+            var candidate = form.elements['candidate[]'];
+            var total=0;
+            for(var i=0; i < candidate.length; i++){
+                if(candidate[i].checked){
+                    total =total +1;}
+                    if(total > 4){
+                    alert("Please Select only four") 
+                    candidate[j].checked = false ;
+                    return false;
+                }
+                if(total==4){
+                    document.getElementById("vote").disabled = false;
+                }else{
+                    document.getElementById("vote").disabled = true;
+                }   
+            }
+        }
+    </script>
 
 @endsection
